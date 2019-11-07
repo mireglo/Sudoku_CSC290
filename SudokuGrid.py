5cd04bc088928244a82ed9b3e6d2527910a1a514
Kyle <kyle.xiao@mail.utoronto.ca>
2019-11-07 17:35:01 -0500

    Implement undo and reset methods

--- SudokuGrid.py
+++ SudokuGrid.py
@@ -1,5 +1,5 @@
 from random import randint, shuffle
-from typing import List, Optional
+from typing import List
 
 
 NUMLIST = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -11,12 +11,24 @@ class SudokuGrid:
     def __init__(self) -> None:
         '''Initializing a SudokuGrid with an empty grid
         '''
-        self._grid = []
+        self._grid_sol = []
+        self._grid_display = []
+        self._difficulty = 2
+        self._move_history = []
         for _ in range(9):
-            self._grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+            self._grid_sol.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+            self._grid_display.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-    def get_cell(self,row, col):
-        return self._grid[row][col]
+    def start_game(self, difficulty):
+        if 0 <= difficulty <= 5:
+            self._difficulty = difficulty
+        self._create_solution()
+        for row in range(9):
+            self._grid_display[row] = self._grid_sol[row][:]
+        self._hide_numbers()
+
+    def get_cell(self, row, col):
+        return self._grid_display[row][col]
 
     @staticmethod
     def check_valid_placement(n: int, row: int, col: int, grid: List) -> bool:
@@ -68,7 +80,7 @@ class SudokuGrid:
                     return False
         return True
 
-    def create_solution(self) -> bool:
+    def _create_solution(self) -> bool:
         '''Recursively constructs a puzzle solution
         '''
         row = 0
@@ -79,21 +91,21 @@ class SudokuGrid:
             col = i % 9
 
             # if cell is empty we try placing number in it
-            if self._grid[row][col] == 0:
+            if self._grid_sol[row][col] == 0:
                 shuffle(NUMLIST)
                 for n in NUMLIST:
 
                     # if n is viable for placement for cell then place it
                     if not SudokuGrid.check_valid_placement(n, row, col,
-                                                            self._grid):
-                        self._grid[row][col] = n
+                                                            self._grid_sol):
+                        self._grid_sol[row][col] = n
 
                         # check if grid is full and return true
-                        if SudokuGrid.check_grid(self._grid):
+                        if SudokuGrid.check_grid(self._grid_sol):
                             return True
 
                         # otherwise recurse to place next cell
-                        elif self.create_solution():
+                        elif self._create_solution():
                             return True
 
                 # break loop if no valid placement in cell
@@ -101,50 +113,50 @@ class SudokuGrid:
 
         # will set current cell to 0 and go back to previous recursion
         # to find another valid cell placement combination
-        self._grid[row][col] = 0
+        self._grid_sol[row][col] = 0
         return False
 
-    def hide_numbers(self):
+    def _hide_numbers(self):
         '''Removes cells while maintaining a unique solution
         '''
         global counter
 
         # num of attempts allow for more blocks to be removed
-        attempts = 2
+        attempts = self._difficulty
 
         while attempts > 0:
             # selecting random cell and rotational counterpart
             row = randint(0, 8)
             col = randint(0, 8)
-            while self._grid[row][col] == 0:
+            while self._grid_display[row][col] == 0:
                 row = randint(0, 8)
                 col = randint(0, 8)
 
             # backing up in case removal is gives multiple solutions
-            backupone = self._grid[row][col]
-            backuptwo = self._grid[8 - row][8 - col]
-            self._grid[row][col] = 0
-            self._grid[8 - row][8 - col] = 0
+            backupone = self._grid_display[row][col]
+            backuptwo = self._grid_display[8 - row][8 - col]
+            self._grid_display[row][col] = 0
+            self._grid_display[8 - row][8 - col] = 0
 
             # cloning grid to test number of solutions
             test_puzzle = []
             for r in range(0, 9):
-                test_puzzle.append(self._grid[r][:])
+                test_puzzle.append(self._grid_display[r][:])
 
             # counter for num solutions is set to 0
             counter = 0
 
             # check num of solutions
-            self.solve_puzzle(test_puzzle)
+            self._solve_puzzle(test_puzzle)
 
             print(counter)
             # if num of solutions is not one, replace the two blocks
             if counter != 1:
-                self._grid[row][col] = backupone
-                self._grid[8 - row][8 - col] = backuptwo
+                self._grid_display[row][col] = backupone
+                self._grid_display[8 - row][8 - col] = backuptwo
                 attempts -= 1
 
-    def solve_puzzle(self, test_puzzle) -> bool:
+    def _solve_puzzle(self, test_puzzle) -> bool:
         '''Attempts to solve puzzle using all possible solution combinations
         '''
         global counter
@@ -174,7 +186,7 @@ class SudokuGrid:
                             break
 
                         # otherwise recurse to place next cell
-                        elif self.solve_puzzle(test_puzzle):
+                        elif self._solve_puzzle(test_puzzle):
                             return True
 
                 # break loop if no valid placement in cell
@@ -188,8 +200,10 @@ class SudokuGrid:
     def change_board(self, n: int, row: int, col: int) -> bool:
         '''Return if move was made successfully
         '''
-        if self._grid[row][col] == 0:
-            self._grid[row][col] = n
+        print(row, col, self.get_cell(row, col))
+        if self._grid_display[row][col] == 0:
+            self._grid_display[row][col] = n
+            self._move_history.append((row, col, n))
             return True
         return False
 
@@ -200,30 +214,48 @@ class SudokuGrid:
         '''
         for row in range(9):
             for col in range(9):
-                n = self._grid[row][col]
-                if not SudokuGrid.check_valid_placement(n, row, col,
-                                                        self._grid):
+                if self._grid_sol[row][col] != self.get_cell(row, col):
                     return False
+        return True
+
+    def reset(self):
+        '''Resetting game
+        '''
+        move_len = len(self._move_history)
+        for _ in range(move_len):
+            self.undo()
+
+    def undo(self):
+        if len(self._move_history) == 0:
+            return False
+        move = self._move_history.pop()
+        self._grid_display[move[0]][move[1]] = 0
         return True
 
     def __str__(self) -> str:
         '''Producing string of matrix for visualization
         '''
         string = ''
-        for row in self._grid:
+        for row in self._grid_display:
             string += row.__str__() + '\n'
         return string
 
 
-if __name__ == "__main__":
-    # initializing empty grid
-    sudokugrid = SudokuGrid()
-    print(sudokugrid)
-
-    # creating puzzle solution
-    sudokugrid.create_solution()
-    print(sudokugrid)
-
-    # hiding numbers to make solvable puzzle
-    sudokugrid.hide_numbers()
-    print(sudokugrid)
+# main method for testing purposes
+# if __name__ == "__main__":
+#     sudoku_grid = SudokuGrid()
+#     sudoku_grid.start_game(3)
+#     print(sudoku_grid)
+#     while not sudoku_grid.game_over():
+#         user = input("u for undo, int for row: ")
+#         if user == "u":
+#             sudoku_grid.undo()
+#         if user == "r":
+#             sudoku_grid.reset()
+#         else:
+#             row = int(user)
+#             col = int(input("Col: "))
+#             n = int(input("Num: "))
+#             sudoku_grid.change_board(n, row, col)
+#
+#         print(sudoku_grid)
