743dca46d40106bdafd04ec22452c876fb319038
Puneet Sandhu <puneetinder.sandhu@mail.utoronto.ca>
2019-11-10 14:16:44 -0500

    Removes get_display_gird in exchange for get_cell The method get_display_gird is removed in exchange for the get_cell method to improve encapsulation of the grid

--- Sudoku.py
+++ Sudoku.py
@@ -10,15 +10,19 @@ class Sudoku:
         self._grid = SudokuGrid()
         self._grid.start_game(5)
 
-    def get_display_grid(self):
-        return self._grid._grid_display
+    # def get_display_grid(self):
+    #     return self._grid._grid_display dont do this
 
     def change_display_grid(self, n, row, col):
         self._grid.change_board(n, row, col)
             #will implement game stop and flash win screen
         return
+
     def restart(self):
-        return
+        return self._grid.reset()
+
+    def get_cell(self, row, col):
+        return self._grid.get_cell(row, col)
 
     def game_over(self):
         return self._grid.game_over()
