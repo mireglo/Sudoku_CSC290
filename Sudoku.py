329ea10b068d14b65b584e352b340603cd7edb12
Puneet Sandhu <puneetinder.sandhu@mail.utoronto.ca>
2019-11-10 13:40:50 -0500

    Rename GridAccess to Sudoku and refactor
    The name is changed to stramline the MVC approach
    Sudoku now acts to encapsulate SudokuGrid

---
+++ Sudoku.py
@@ -0,0 +1,24 @@
+"""
+The purpose of this class is to extract a grid from SudokuGrid and use it as
+the and to hold the playergrid
+"""
+from SudokuGrid import*
+
+
+class Sudoku:
+    def __init__(self):
+        self._grid = SudokuGrid()
+        self._grid.start_game(5)
+
+    def get_display_grid(self):
+        return self._grid._grid_display
+
+    def change_display_grid(self, n, row, col):
+        self._grid.change_board(n, row, col)
+            #will implement game stop and flash win screen
+        return
+    def restart(self):
+        return
+
+    def game_over(self):
+        return self._grid.game_over()
